4b78075076a79f7a46d15d9e96bca03fcded29a9
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-02-09 16:55:04 -0300

    Update main.py

--- DataLogger/main.py
+++ DataLogger/main.py
@@ -40,28 +40,6 @@ while True:
         else:
             print("Continuando...")
 
-
-
-
-
-    # #Envio de msg UDP
-    #
-    # escrever(arq, "UDP Send")
-    # print(f'{str(datetime.today())[0:str(datetime.today()).find(".")]} -> UDP Send')
-    #
-    # client_socket.sendto(bytesToSend, serverAddressPort)  # Send the data request
-    # sleep(3)
-    # try:
-    #     rec_data1, addr = client_socket.recvfrom(2048)  # Read response from arduino
-    #
-    #     if (rec_data1 != " "):
-    #         escrever(arq, str(rec_data1))
-    #         print(f'{str(datetime.today())[0:str(datetime.today()).find(".")]} -> {rec_data1}')
-    #
-    # except:
-    #     print(f'{str(datetime.today())[0:str(datetime.today()).find(".")]} -> Erro na recepcao')
-    #     pass
-
 # Finaliza a serial
 serial.closeSerial()
 print("Finalizado o programa")
