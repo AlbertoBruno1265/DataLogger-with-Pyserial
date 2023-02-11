07f99f7b6ee0a916caa3aabbc6be2c23066d67fb
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-02-11 16:00:25 -0300

    Update SERIAL_PORT_TESTER.ino

--- SERIAL_PORT_TESTER/SERIAL_PORT_TESTER.ino
+++ SERIAL_PORT_TESTER/SERIAL_PORT_TESTER.ino
@@ -6,15 +6,18 @@
     GITHUB: 
  */
 
- int seconds = 0;
+int seconds = 0;
+int delayMs = 1000;
 
 void setup() {
+  // Open Serial port and set speed
   Serial.begin(9600);
   }
 
 void loop() {
-  delay(1000);
+  // Print to serial every second
+  delay(delayMs);
   seconds += 1;
-  Serial.println("Teste");
+  Serial.println("Hello World!");
   Serial.print("Seconds: "); Serial.println(seconds);
 }
